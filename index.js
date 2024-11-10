b83f1268027fcd6198acfadc09633d643b17d4d2
Ashish Raj <ashishraj09@gmail.com>
2024-11-10 20:11:55 +1300

    Remove cors change

--- index.js
+++ index.js
@@ -31,12 +31,6 @@ const corsOptions = {
 };
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", originUrl);
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  next();
-});
 
 const https = require("https");
 const agent = new https.Agent({
